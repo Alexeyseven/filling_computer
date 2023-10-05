c29c357e3bbbc6bf041eaed1ed92bf1edf37dc85
Alexeyseven <alexeyseven77@gmail.com>
2023-10-05 16:30:01 +0300

    shift_register

--- shift_register.py
+++ shift_register.py
@@ -3,6 +3,7 @@ import math
 import time
 import socket
 import os
+from register import register
 
 
 RES = WIDTH, HEIGHT = 400, 400
@@ -15,7 +16,6 @@ surface = pygame.display.set_mode(RES)
 clock60 = dict(zip(range(360), range(0, 360, 1)))
 
 encoder = 0
-register = [[False, 0, 0], [False, 10, 0], [False, 20, 0], [False, 30, 0]]
 
 s = socket.socket()
 s.bind(('192.168.1.241', 2000))
@@ -62,9 +62,23 @@ while True:
     text1 = font.render(f'{str(register[0][2])}', True, (180, 0, 0))
     text2 = font.render(f'{str(register[1][2])}', True, (180, 0, 0))
     text3 = font.render(f'{str(register[2][2])}', True, (180, 0, 0))
+    text4 = font.render(f'{str(register[3][2])}', True, (180, 0, 0))
+    text5 = font.render(f'{str(register[4][2])}', True, (180, 0, 0))
+    text6 = font.render(f'{str(register[5][2])}', True, (180, 0, 0))
+    text7 = font.render(f'{str(register[6][2])}', True, (180, 0, 0))
+    text8 = font.render(f'{str(register[7][2])}', True, (180, 0, 0))
+    text9 = font.render(f'{str(register[8][2])}', True, (180, 0, 0))
+    text10 = font.render(f'{str(register[9][2])}', True, (180, 0, 0))
     surface.blit(text1, (10, 10))
     surface.blit(text2, (10, 40))
     surface.blit(text3, (10, 70))
+    surface.blit(text4, (10, 100))
+    surface.blit(text5, (10, 130))
+    surface.blit(text6, (10, 160))
+    surface.blit(text7, (10, 190))
+    surface.blit(text8, (10, 220))
+    surface.blit(text9, (10, 250))
+    surface.blit(text10, (10, 270))
     
     pygame.draw.circle(surface, pygame.Color('darkslategray'), (H_WIDTH, H_HEIGHT), RADIUS)
     pygame.draw.line(surface, pygame.Color('green'), (H_WIDTH, H_HEIGHT), get_clock_pos(clock60, 0), 2)
